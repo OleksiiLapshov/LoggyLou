5887345e011cd5de53da4c35b08f4d31bf6f3079
OleksiiLapshov <67839861+OleksiiLapshov@users.noreply.github.com>
2025-10-06 20:08:23 +0300

    added seed to make it easier to use on different machines

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.destroy_all
+Project.destroy_all
+Worklog.destroy_all
+
+User.create!([
+  {
+    first_name: "admin",
+    last_name: "dude",
+    email: "admin@loggylou.com",
+    password: "admin",
+    password_confirmation: "admin",
+    admin: true
+  },
+  {
+    first_name: "Lou",
+    last_name: "Cat",
+    email: "cat@loggylou.com",
+    password: "qwerty",
+    password_confirmation: "qwerty"
+  },
+  {
+    first_name: "Test",
+    last_name: "Tester",
+    email: "test@loggylou.com",
+    password: "qwerty",
+    password_confirmation: "qwerty"
+  }
+])
+
+Project.create!([
+  {
+    name: "Default",
+    company: "OkayQuai"
+  },
+  {
+    name: "Test",
+    company: "Test"
+  },
+  {
+    name: "Work",
+    company: "Work ltd"
+  }
+])
