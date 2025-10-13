b060520a802cbcae756ac708420612f9a0b53c10
OleksiiLapshov <67839861+OleksiiLapshov@users.noreply.github.com>
2025-10-13 19:24:50 +0300

    added some through associations to test how it works (users to project and projects to users)

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,7 @@ class Project < ApplicationRecord
   has_many :worklogs, dependent: :destroy
   has_many :assignments, dependent: :destroy
 
+  has_many :users, through: :assignments, source: :user
+
   validates :name, :company, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :worklogs, dependent: :destroy
   has_many :assignments, dependent: :destroy
 
+  has_many :projects, through: :assignments, source: :project
+
   validates :first_name, :last_name, presence: true
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
 end
