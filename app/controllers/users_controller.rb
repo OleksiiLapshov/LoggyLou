cf65a730dfec5621ea38df12af3d356d6dd318d8
Oleksii Lapshov <izraphail@gmail.com>
2025-10-14 23:45:11 +0300

    added checkboxes to assign projects for every user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,8 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, 
+                                :password, :password_confirmation,
+                                project_ids: [])
   end
 end
