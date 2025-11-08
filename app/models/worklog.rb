6d12ed7fbb98d6440aebb55538b5da606278dabb
Oleksii Lapshov <izraphail@gmail.com>
2025-11-08 15:52:54 +0200

    added some helpers and scopes

--- app/models/worklog.rb
+++ app/models/worklog.rb
@@ -5,4 +5,16 @@ class Worklog < ApplicationRecord
   validates :hours, :note, :project, :log_date, presence: true
 
   belongs_to :submission, optional: true
+
+  scope :not_submitted, -> { where(submission_id: nil) }
+
+  scope :for_period, ->(month, year) {
+    return none unless mpnth && year
+    date = Date.new(year.to_i, month.to_1, 1)
+    where(log_date: date.beginning_of_month..date.end_of_month)
+  }
+
+  def editable?
+    submission_id.nil? || submission&.draft?
+  end
 end
