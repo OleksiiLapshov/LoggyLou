08d1e7452b809cc1fe4967d67dc4843eeab5d611
OleksiiLapshov <67839861+OleksiiLapshov@users.noreply.github.com>
2025-11-24 21:14:32 +0200

    critical improvements on session creation/destroy action to prevent certain attacks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    email_input = params[:email].to_s.downcase.strip
+    user = User.find_by(email: email_input)
+
     if user && user.authenticate(params[:password])
+      reset_session
       session[:user_id] = user.id
       redirect_to worklogs_url, notice: "Welcome back, #{user.first_name}!"
     else
+      unless user
+        User.new(password: "placeholder").authenticate('invalid')
+      end
       flash.now[:alert] = "Invalid email/password combination!"
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    session[:user_id] = nil
+    reset_session
     redirect_to signin_path, status: :see_other, notice: "Signed out!"
   end
 end
