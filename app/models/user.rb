dfcd6d83c5198f1da130a7a2dec36e8dba16f77c
OleksiiLapshov <67839861+OleksiiLapshov@users.noreply.github.com>
2025-11-25 11:06:06 +0200

    improved password validation to reflecr minimal standards

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,39 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, allow_nil: true
+  validate :password_complexity
 
   def full_name
     "#{first_name} #{last_name}".strip
+  end
+
+
+  private
+
+  def password_complexity
+    # skip validation if there is no password (on update)
+    return if password.blank?
+
+    # check for uppercase
+    unless password.match?(/[A-Z]/)
+      errors.add :password, 'must include at least one uppercase letter'
+    end
+
+    # check for lowercase
+    unless password.match?(/[a-z]/)
+      errors.add :password, 'must include at least one lowercase letter'
+    end
+
+    # check for digits
+    unless password.match?(/[0-9]/)
+      errors.add :password, 'must include at least one digit'
+    end
+
+    # check for special characters
+    # crutch to avoid whitelist: this regex looks for anything that is NOT a letter or number
+    unless password.match?(/[^a-zA-Z0-9]/)
+      errors.add :password, 'must include at least one special character'
+    end
   end
 end
