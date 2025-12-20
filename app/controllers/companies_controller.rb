94866c476d04a0b8d9c8fbfc4c6f8a8a29f83138
OleksiiLapshov <67839861+OleksiiLapshov@users.noreply.github.com>
2025-12-20 18:59:18 +0200

    added form to edit company details

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,12 +1,12 @@
 class CompaniesController < ApplicationController
   before_action :require_signin
   before_action :require_admin
+  before_action :set_company, only: %i[ show edit update ]
   def index
     @companies = Company.includes(:projects)
   end
 
   def show
-    @company = Company.find(params.expect(:id))
   end
 
   def new
@@ -19,5 +19,23 @@ class CompaniesController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @company.update(company_params)
+        format.html { redirect_to @company, notice: "Company details were successfully updated.", status: :see_other }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
   end
+
+private
+    def set_company
+      @company = Company.find(params.expect(:id))
+    end
+
+    def company_params
+      params.expect(company: [ :name, :address_line_1, :address_line_2,
+                               :postcode, :city, :country, :vat, :account_number,
+                               :contact_phone, :contact_email, :contact_person ])
+    end
 end
